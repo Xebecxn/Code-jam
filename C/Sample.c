4216873ea7bc6d76f219c481e65eea1d7a8e24ff
Xebecx <147933037+Xebecxn@users.noreply.github.com>
2023-10-20 22:19:22 +0530

    Create Sample.c

---
+++ C/Sample.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main()
+{
+  prinf("Helloooo World ")
+//Comment where necessary 
+//In this way you can create new folder and add your code
+//Thanks for watching :) 
+  
+}
